549d66e3dd9c6714be51d2f7fb5d67f39062b5e2
drunken_neoguri <developneoguri@gmail.com>
2024-09-03 20:42:29 +0900

    📦Chore: Nav Bar 아이콘 리스트 추가 #5

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,7 @@
+import HomeIcon from './icon-home.svg';
+import MyPageIcon from './icon-mypage.svg';
+import SearchIcon from './icon-search.svg';
+import WriteIcon from './icon-write.svg';
+
+// ?CONCERN: 임시로 적용 -> 이전 논의로는 svg도 public으로 하는 것으로 알고 있는데 어떻게 할까요?
+export { HomeIcon, MyPageIcon, SearchIcon, WriteIcon };
