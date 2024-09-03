9b9eeadc64c14c05bcaf86611a85dae34088248f
drunken_neoguri <developneoguri@gmail.com>
2024-09-03 20:45:25 +0900

    ✨Feat: NavBar 스타일 반영 처리  #5

--- src/components/component/nav-bar/index.tsx
+++ src/components/component/nav-bar/index.tsx
@@ -1,6 +1,11 @@
-import Button from '../button';
-import { useRouter } from 'next/router';
 import type { MouseEvent } from 'react';
+import { useRouter } from 'next/router';
+
+import Button from '../button';
+
+import styles from './style.module.scss';
+
+import { HomeIcon, MyPageIcon, SearchIcon, WriteIcon } from '@/assets/icons/icons';
 
 export default function NavBar() {
   const router = useRouter();
@@ -11,17 +16,20 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="navbar">
+    <nav className={styles.navBar}>
       <Button id="main" buttonType="nav" onClick={handleMoveToPage}>
-        홈
+        <HomeIcon />홈
       </Button>
       <Button id="search" buttonType="nav" onClick={handleMoveToPage}>
+        <SearchIcon />
         검색
       </Button>
       <Button id="write" buttonType="nav" onClick={handleMoveToPage}>
+        <WriteIcon />
         작성
       </Button>
       <Button id="mypage" buttonType="nav" onClick={handleMoveToPage}>
+        <MyPageIcon />
         마이페이지
       </Button>
     </nav>
