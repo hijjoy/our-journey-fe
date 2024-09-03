dbec30839fd1d4adc82d24fce0fd892e0607f75f
drunken_neoguri <developneoguri@gmail.com>
2024-09-03 20:44:34 +0900

    ✨Feat: 공용 버튼 내 button type lint 무시되도록 처리 #5

--- src/components/component/button/index.tsx
+++ src/components/component/button/index.tsx
@@ -1,5 +1,6 @@
-import styles from './style.module.scss';
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import styles from './style.module.scss';
 
 interface InterfaceButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonType?: 'base-small' | 'base-medium' | 'base-large' | 'base-full' | 'logo' | 'sort' | 'icon' | 'nav';
@@ -10,6 +11,7 @@ export default function Button(props: InterfaceButton) {
   const { children, buttonType = 'base-full', type = 'button', ...rest } = props;
 
   return (
+    // eslint-disable-next-line react/button-has-type
     <button className={styles[buttonType]} type={type} {...rest}>
       {children}
     </button>
