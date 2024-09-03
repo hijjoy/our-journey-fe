c9e059223127c38cada34b94d498f5c4420be916
drunken_neoguri <developneoguri@gmail.com>
2024-09-03 20:39:41 +0900

    ✨Feat: 버튼 class 반영 방식 변경 #5

--- src/components/component/button/index.tsx
+++ src/components/component/button/index.tsx
@@ -1,34 +1,16 @@
+import styles from './style.module.scss';
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface InterfaceButton extends ButtonHTMLAttributes<HTMLButtonElement> {
-  buttonType?: 'base' | 'logo' | 'sort' | 'icon' | 'nav';
+  buttonType?: 'base-small' | 'base-medium' | 'base-large' | 'base-full' | 'logo' | 'sort' | 'icon' | 'nav';
   children: ReactNode;
-  size?: 's' | 'm' | 'l' | 'full';
 }
 
 export default function Button(props: InterfaceButton) {
-  const { children, buttonType = 'base', size = 'full', type = 'button', className, ...rest } = props;
-
-  const buttonStyle = {
-    base: 'base ',
-    icon: 'icon ',
-    nav: 'nav ',
-    logo: 'logo ',
-    sort: 'sort ',
-  };
-
-  const baseSize = () => {
-    if (buttonStyle[buttonType] === 'base') {
-      if (size === 's') return 'small ';
-      if (size === 'm') return 'medium ';
-      if (size === 'l') return 'large ';
-      if (size === 'full') return 'full ';
-    }
-    return '';
-  };
+  const { children, buttonType = 'base-full', type = 'button', ...rest } = props;
 
   return (
-    <button className={buttonStyle[buttonType] + baseSize() + className} type={type} {...rest}>
+    <button className={styles[buttonType]} type={type} {...rest}>
       {children}
     </button>
   );
